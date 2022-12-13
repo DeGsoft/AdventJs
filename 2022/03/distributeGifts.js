47a5e9ca447506a692651a9336d1fcf5a7772a4e
DeGssoft <diegoezequielguillen@gmail.com>
2022-12-13 00:41:38 -0300

    03

---
+++ 2022/03/distributeGifts.js
@@ -0,0 +1,6 @@
+function distributeGifts(packOfGifts, reindeers) {
+    return Math.trunc(reindeers.reduce((acc, cur) =>
+        acc + cur.length * 2, 0)
+        / packOfGifts.reduce((acc, cur) =>
+            acc + cur.length, 0));
+}
